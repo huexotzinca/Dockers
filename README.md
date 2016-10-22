091f6c72c7ac7215594d61366628f1ac28a23abb
Neftali Bautista <dev@neftalibautista.com>
2016-10-21 19:36:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Huexotzinca Dockers
+
+[![N|Solid](https://www.docker.com/sites/default/files/legal/small_h.png)](https://www.docker.com/)
+
+This repo contains all Dockerfiles for automaticbuild used in [Huexotzinca's containers](https://hub.docker.com/u/huexotzinca/)
---
+++ ubuntu/development/media/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+</head>
+<body>
+<h1>Welcome</h1>
+<p>The tools has been installede succesfull, congrats. Now you can enjoy:</p>
+<ul>
+    <li>Apache2</li>
+    <li>PHP</li>
+    <li>MySQL</li>
+    <li>PostgreSQL</li>
+    <li>Node</li>
+    <li>Ruby</li>
+</ul>
+</body>
+</html>
