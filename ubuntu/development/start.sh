be3ebaee0d9cd96eccb810b086d6c8abe0e4c1d2
Neftali Bautista <dev@neftalibautista.com>
2017-03-28 18:58:54 -0600

    Version udv0.1.0 released :boom: :smiley:

--- ubuntu/development/start.sh
+++ ubuntu/development/start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-#Enable ssh, apache and mysql on start
+# Enable ssh, apache, mysql and postgresql on container start
 service ssh start
 apache2ctl -k start
 service mysql start
+sudo service postgresql stop
+service postgresql start
+
+## Uncomment the next code if you like autoupdate from gem and node on Start
+# su docker
+# gem update && npm -g update
+
 /bin/bash
